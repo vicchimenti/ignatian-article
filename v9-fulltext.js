e70f2675de0d23fb5a14b906358771cd306eaabf
Vic Chimenti <victor.chimenti@gmail.com>
2021-08-26 08:51:49 -0700

    initializations

--- v9-fulltext.js
+++ v9-fulltext.js
@@ -5,7 +5,7 @@
    *
    *     Document will write once when the page loads
    *
-   *     @version 5.94
+   *     @version 6.0
    */
 
 
@@ -26,14 +26,25 @@
       var articleImage = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Image' output='normal' formatter='path/*' />");
       var articleImageAlt = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='media' name='Image' attribute='description' />");
       var externalLink = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='External Link' output='normal' modifiers='striptags,htmlentities' />");
-      var externalLinkText = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Journal' output='normal' modifiers='striptags,htmlentities' />");
+     
+      var journal = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Journal' output='normal' modifiers='striptags,htmlentities' />");
+    //   var externalLinkText = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Journal' output='normal' modifiers='striptags,htmlentities' />");
+    
       var topics = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Topic' output='normal' display_field='value' modifiers='striptags,htmlentities' />");
       var topicSetting = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Setting' output='normal' display_field='value' modifiers='striptags,htmlentities' />");  
+      
+      var courses = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Course' output='normal' display_field='value' modifiers='striptags,htmlentities' />");
+      var level = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Level' output='normal' display_field='value' modifiers='striptags,htmlentities' />");
+      var program = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Program' output='normal' display_field='value' modifiers='striptags,htmlentities' />");
+      var resource = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Resource' output='normal' display_field='value' modifiers='striptags,htmlentities' />");
+      var author = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Author' output='normal' modifiers='striptags,htmlentities' />");
+
       var articleFullBody = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Article Body' output='normal' display_field='value' />");
       var contentID = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='meta' meta='content_id' />");
       var anchorTag = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='meta' meta='html_anchor' />");
 
 
+  
 
 
       /***
@@ -55,10 +66,25 @@
       var imageString = '<img class="hidden visually-hidden" />';
       var dateString = '<p class="publishDate card-text"><small>' + publishDate + '</small></p>';
       var externalLinkString = '<p class="externalLink hidden">No Proper Link Provided</p>';
-      var listOfTags = '<div class="newsroomArticle tags hidden visually-hidden"><ul></ul></div>';
+
+      var listOfTags = '<div class="newsroomArticle tags pedagogy hidden visually-hidden"><ul class="categories"><li class="tag">No Pedagogy Provided</li></ul></div>';
+      var listOfCourses = '<div class="newsroomArticle tags courses hidden visually-hidden"><ul class="categories"><li class="tag">No Course Type Provided</li></ul></div>';
+      var listOfLevels = '<div class="newsroomArticle tags levels hidden visually-hidden"><ul class="categories"><li class="tag">No Level Provided</li></ul></div>';
+      var byLine = '<p class="card-text text-muted font-weight-light author hidden visually-hidden">No Author Provided</p>';
+
+
       var listItems = '';
       var topicSettingString = '<p class="card-text setting hidden visually-hidden">No Setting Provided</p>';
 
+      var resourceString = '<p class="card-text resource hidden visually-hidden">No Resource Provided</p>';
+      var programString = '<p class="card-text program hidden visually-hidden">No Program Provided</p>';
+
+
+      var openRow = '<div class="row no-gutters">';
+      var closeRow = '</div>';
+
+
+  
 
 
 
@@ -66,9 +92,9 @@
        *  Parse for external link
        * 
        * */
-      if (externalLink != "" && externalLinkText != "") {
-          externalLinkString = '<p class="externalLink card-text"><a href="' + externalLink + '" title="' + externalLinkText + '" target="_blank" class="card-link"><em>' + externalLinkText + '</em></a></p>';
-      }
+    //   if (externalLink != "" && externalLinkText != "") {
+    //       externalLinkString = '<p class="externalLink card-text"><a href="' + externalLink + '" title="' + externalLinkText + '" target="_blank" class="card-link"><em>' + externalLinkText + '</em></a></p>';
+    //   }
 
 
       /***
@@ -170,6 +196,186 @@
       document.write(endingHTML);
 
 
+
+
+
+          /***
+     *  default declarations
+     * 
+     * */
+
+      
+
+
+    /***
+     *  default initializations
+     * 
+     * */
+
+
+
+
+    /***
+     *  check for fulltext content
+     * 
+     * */
+    if (articleFullBody != "") {
+        titleLink = '<h3><a href="' + fullTextLink + '" title="Read the full post ' + articleTitle + '">' + articleTitle + '</a></h3>';
+    }
+
+
+
+
+    /***
+     *  Parse for external link
+     * 
+     * */
+    if (externalLink != "" && journal != "") {
+        externalLinkString = '<span class="externalLink"><a href="' + externalLink + '" title="' + journal + '" target="_blank"><em>' + journal + '</em></a></span>';
+    }
+
+
+
+
+    /***
+     *  Parse for image
+     * 
+     * */
+    if (articleImage != "") {
+        openImageWrapper = '<div class="col-md-4">';
+        imageString = '<img src="' + articleImage + '" class="articleImage card-img" alt="' + articleImageAlt + '" />';
+    }
+
+
+
+
+    /***
+     *  Parse for author
+     * 
+     * */
+    if (author != "") {
+        byLine = '<p class="card-text author"><strong>By ' + author + '</strong></p>';
+    }
+
+
+
+
+    /***
+     *  Parse for Setting
+     * 
+     * */
+    if (topicSetting != "") {
+        topicSettingString = '<p class="card-text setting"><strong class="articleLabel">Setting: </strong>' + topicSetting + '</p>';
+    }
+
+
+
+
+    /***
+     *  Parse for Resource
+     * 
+     * */
+    if (resource != "") {
+        resourceString = '<p class="card-text resource"><strong class="articleLabel">Activity Type: </strong><span class="resourceType">' + resource + '</span></p>';
+    }
+
+
+
+
+    /***
+     *  Parse for Program Type
+     * 
+     * */
+    if (program != "") {
+        programString = '<p class="card-text program"><strong class="articleLabel">Program Type: </strong><span class="programType">' + program + '</span></p>';
+    }
+
+
+
+
+    /***
+     *  parse the list of level tags, add <li> tags
+     * 
+     * */
+    if (level != "") {
+
+        let levelItems = '';
+        let arrayOfLevels = level.split(',');
+        for (let i = 0; i < arrayOfLevels.length; i++) {
+            levelItems += '<li class="tag">' + arrayOfLevels[i].trim() + '</li>';
+        }
+
+        // Print any tags that were selected
+        listOfLevels = '<div class="newsroomArticle tags levels"><strong class="card-text articleLabel">Level: </strong><ul class="categories">' + levelItems + '</ul></div><br>';
+    }
+
+
+
+
+    /***
+     *  parse the list of courses tags, add <li> tags
+     * 
+     * */
+    if (courses != "") {
+
+        let courseItems = '';
+        let arrayOfCourses = courses.split(',');
+        for (let i = 0; i < arrayOfCourses.length; i++) {
+            courseItems += '<li class="tag">' + arrayOfCourses[i].trim() + '</li>';
+        }
+
+        // Print any tags that were selected
+        listOfCourses = '<div class="newsroomArticle tags courses"><strong class="card-text articleLabel">Course: </strong><ul class="categories">' + courseItems + '</ul></div><br>';
+    }
+
+
+
+
+    /***
+     *  parse the list of topics tags, add <li> tags
+     * 
+     * */
+    if (topics != "") {
+
+        let listItems = '';
+        let arrayOfTags = topics.split(',');
+        for (let i = 0; i < arrayOfTags.length; i++) {
+            listItems += '<li class="tag">' + arrayOfTags[i].trim() + '</li>';
+        }
+
+        // Print any tags that were selected
+        listOfTags = '<div class="newsroomArticle tags pedagogy"><strong class="card-text articleLabel">Ignatian Pedagogy: </strong><ul class="categories">' + listItems + '</ul></div>';
+    }
+
+
+
+
+    /***
+     *  write document once
+     * 
+     * */
+    document.write(beginningHTML);
+    document.write(titleLink);
+    document.write(openRow);
+    document.write(openImageWrapper);
+    document.write(imageString);
+    document.write(closeImageWrapper);
+    document.write(openBodyWrapper);
+    document.write(externalLinkString);
+    document.write(openSummaryWrapper);
+    document.write(byLine);
+    document.write(summaryString);
+    document.write(dateString);
+    document.write(topicSettingString);
+    document.write(resourceString);
+    document.write(programString);
+    document.write(listOfLevels);
+    document.write(listOfCourses);
+    document.write(listOfTags);
+    document.write(closeSummaryWrapper);
+    document.write(closeBodyWrapper);
+    document.write(closeRow);
+    document.write(endingHTML);
 
 
   } catch (err) {
