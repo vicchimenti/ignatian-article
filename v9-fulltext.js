6dca35003b0f6e99e8a9d77d6d0f304e02d1e7a5
Vic Chimenti <victor.chimenti@gmail.com>
2021-08-26 09:06:10 -0700

    clean

--- v9-fulltext.js
+++ v9-fulltext.js
@@ -177,26 +177,22 @@
     }
 
 
-
-
-
-
-
-        /***
-         *  parse the list of topics tags, add <li> tags
-         * 
-         * */
-         if (topics != "") {
     
-            let listItems = '';
-            let arrayOfTags = topics.split(',');
-            for (let i = 0; i < arrayOfTags.length; i++) {
-                listItems += '<li class="tag">' + arrayOfTags[i].trim() + '</li>';
-            }
-    
-            // Print any tags that were selected
-            listOfTags = '<div class="newsroomArticle tags pedagogy"><strong class="card-text articleLabel">Ignatian Pedagogy Element: </strong><ul class="categories">' + listItems + '</ul></div>';
+    /***
+     *  parse the list of topics tags, add <li> tags
+     * 
+     * */
+    if (topics != "") {
+
+        let listItems = '';
+        let arrayOfTags = topics.split(',');
+        for (let i = 0; i < arrayOfTags.length; i++) {
+            listItems += '<li class="tag">' + arrayOfTags[i].trim() + '</li>';
         }
+
+        // Print any tags that were selected
+        listOfTags = '<div class="newsroomArticle tags pedagogy"><strong class="card-text articleLabel">Ignatian Pedagogy Element: </strong><ul class="categories">' + listItems + '</ul></div>';
+    }
 
     
     
